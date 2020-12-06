299fcdf747e8ebac7225425a7f9cded66b28a0ed
Uli <52220881+Gepeusea@users.noreply.github.com>
2020-12-06 10:57:18 +0300

    Отредактированы отступы

--- server.py
+++ server.py
@@ -8,13 +8,13 @@ cardsuit = ["черви","вини","буби","крести"]
 cardvalue = ["2","3","4","5","6","7","8","9","10","Валет","Дама","Король","Туз"]
 
 class Card:
-    
-    __slots__ = ('value', 'suit')
-
-    def __init__(self, dsuit, dval):
-        self.value = dval
+	
+	__slots__ = ('value', 'suit')
+	
+	def __init__(self, dsuit, dval):
+		self.value = dval
 		self.suit = dsuit
-
+		
 	def get_card_value(self):
 		return self.value
 
@@ -25,11 +25,11 @@ class Card:
 		return cardvalue[self.value] + " " + cardsuit[self.suit]
 
 class Player:
-    
-    __slots__ = ('stack', 'nickname')
-
-    def __init__(self, newstack, newnickname):
-        self.stack = newstack
+	
+	__slots__ = ('stack', 'nickname')
+	
+	def __init__(self, newstack, newnickname):
+		self.stack = newstack
 		self.nickname = newnickname
 		self.with_cards = False
 		self.hand_cards = {}
